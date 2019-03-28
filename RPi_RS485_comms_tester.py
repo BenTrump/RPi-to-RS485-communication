df664d7f5ea74236f08f9ebccaa0670dc8db8b48
BenTrump <bmtseducation@gmail.com>
2019-03-28 15:21:05 -0700

    Initial commit

---
+++ RPi_RS485_comms_tester.py
@@ -0,0 +1,12 @@
+
+from pymodbus.client.sync import ModbusSerialClient as ModbusClient
+from pymodbus.register_read_message import ReadInputRegistersResponse
+
+client = ModbusClient(method="rtu", port="/dev/ttyUSB0", stopbits=1,
+                      bytesize=9, parity="none", baudrate="9600", timeout=0.3)
+
+connection = client.connect()
+print(connection)
+
+value = client.read_input_registers(6, 4, unit=0x01)
+print(value.registers)
