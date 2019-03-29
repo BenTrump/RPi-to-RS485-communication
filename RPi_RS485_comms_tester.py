b717d54cb13a6b9bdda44bd854dbd64a84101908
BenTrump <bmtseducation@gmail.com>
2019-03-29 10:59:21 -0700

    Added functionality for reading coils, input status, and holding reg

--- RPi_RS485_comms_tester.py
+++ RPi_RS485_comms_tester.py
@@ -8,5 +8,12 @@ client = ModbusClient(method="rtu", port="/dev/ttyUSB0", stopbits=1,
 connection = client.connect()
 print(connection)
 
-value = client.read_input_registers(6, 4, unit=0x01)
-print(value.registers)
+read_coils = client.read_coils(1, 4, unit=0x01)
+read_input_status = client.read_discrete_inputs(1, 4, unit=0x01)
+read_holding_reg = client.read_holding_registers(1, 4, unit=0x01)
+read_input_reg = client.read_input_registers(6, 4, unit=0x01)  # Start register, num of registers to read, slave ID
+print("From input registers: " + read_input_reg.registers)
+print("From coils: " + read_coils.registers)
+print("From input status: " + read_input_status.registers)
+print("From holding registers: " + read_holding_reg.registers)
+
